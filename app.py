d8abd5b7271612262b77a946b6bbba6e89e6c6f1
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-12-06 18:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,12 +88,19 @@ st.image("vllkyt19n98psusds8.jpg", use_column_width=True)
 # description about the project and code files            
 
 st.subheader("🧾Description:")
-st.text("This data set is collected from Addis Ababa Sub-city police departments for master's research work. The data set has been prepared from manual records of road traffic accidents of the year 2017-20. All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident. Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.")
+st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
+The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
+All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
+Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
+""")
 
 st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
 
 st.subheader("🧭 Problem Statement:")
-st.text("The target feature is Accident_severity which is a multi-class variable. The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. the metric for evaluation will be f1-score")
+st.text("""The target feature is Accident_severity which is a multi-class variable. 
+The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
+The metric for evaluation will be f1-score
+""")
 
 st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")                  
                   
