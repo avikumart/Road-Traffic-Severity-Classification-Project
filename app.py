53c6dade3959fbb125a252bfbfaa3b769e935bc3
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-12-06 17:39:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def main():
                      st.write(f"The severity prediciton is slight injury")
 
 # post the image of the accident
-st.image(np.array("vllkyt19n98psusds8.jpg"))
+st.image("vllkyt19n98psusds8.jpg")
 
 # description about the project and code files                  
 st.markdown("Please find GitHub repository link of project: [Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")                  
